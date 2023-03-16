9f09f573fcc9c21683c7ddd2818ff027a6592f32
Filipa Merino <filipaam@outlook.com>
2023-03-16 16:46:31 +0000

    database set

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const { urlencoded } = require("body-parser");
+const https = require("https");
 
 const app = express();
 app.use(express.static("public"));
@@ -21,21 +22,38 @@ app.post("/", function(req, res){
   const clientEmail = req.body.email;
 
   const data = {
-    members: [{
+    members: [
+      {
       email_address: clientEmail,
       status: "subscribed",
       merge_fields: {
         FNAME: firstName,
         LNAME: lastName
+        }
       }
-    }]
+    ]
   }
 
-  const jsonData = JSON(data.stringify(data));
+  const jsonData = JSON.stringify(data);
+  const url = "https://us13.api.mailchimp.com/3.0/lists/7211e69e97";
+
+  const options = {
+      method: "POST",
+      auth: "filipa1:cdb6cacf2cd0286884aa6a2a971a622d-us13"
+  }
+
+
+  const request = https.request(url, options, function(response){
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
 
 });
-
-
 
 app.listen(3000, function(){
   console.log("You're in!");
@@ -45,4 +63,4 @@ app.listen(3000, function(){
 // 7211e69e97
 
 // API Key
-// a7b45baf71ce22c21ad01b65f9ef7990-us13
+// cdb6cacf2cd0286884aa6a2a971a622d-us13
