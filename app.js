4dd877a448c749559d10e46128393ec75fc5a7ae
Filipa Merino <filipaam@outlook.com>
2023-03-16 15:11:22 +0000

    post method

--- app.js
+++ app.js
@@ -3,14 +3,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const { urlencoded } = require("body-parser");
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true }));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
 
 })
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var clientEmail = req.body.email;
+
+});
+
+
 
 app.listen(3000, function(){
   console.log("You're in!");
