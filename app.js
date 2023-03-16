2d167e21f115f1fef2917283a51d67e83253e87e
Filipa Merino <filipaam@outlook.com>
2023-03-16 17:24:45 +0000

    redirecting to pages

--- app.js
+++ app.js
@@ -35,20 +35,20 @@ app.post("/", function(req, res){
   }
 
   const jsonData = JSON.stringify(data);
-  const url = "https://us13.api.mailchimp.com/3.0/lists/7211e69e97";
+  const url = "https://us13.api.mailchimp.com/3.0/lists/7211e69e9";
 
   const options = {
       method: "POST",
-      auth: "filipa1:cdb6cacf2cd0286884aa6a2a971a622d-us13"
+      auth: "filipa1:a04ee7a624bfbc3e08bd9c392f2145c2-us13"
   }
 
 
   const request = https.request(url, options, function(response){
 
     if (response.statusCode === 200) {
-      res.send("Sucessfully subscribed!")
+      res.sendFile(__dirname + "/success.html")
     } else {
-      res.send("There was an error on your signing up :(");
+      res.sendFile(__dirname + "/failure.html");
     }
 
     response.on("data", function(data){
@@ -60,6 +60,19 @@ app.post("/", function(req, res){
   request.end();
 
 });
+
+
+
+app.post("/failure", function(req, res){
+  console.log(req.body.tryAgain);
+
+  // button.on("click", function(){
+  //   res.redirect('/')
+  // })
+});
+
+
+
 
 app.listen(3000, function(){
   console.log("You're in!");
