43b038eb73f7e6b942ebd5cec7fdd47e2939d8ce
Filipa Merino <filipaam@outlook.com>
2023-03-16 14:58:16 +0000

    link resources (css + images) to app.js

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
-
-
 const app = express();
-
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.send("You're in!");
+  res.sendFile(__dirname + "/signup.html");
+
 })
 
 app.listen(3000, function(){
